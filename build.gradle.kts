1a4ef1f50b970ce4b792858263255afe95ff5b57
Roman Elizarov <elizarov@gmail.com>
2024-10-01 20:16:23 +0300

    2024 version Java 21 Kotlin 2.0.20 Gradle 8.10.1 Lincheck 2.34

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "2.0.20"
     application
+}
+
+tasks {
+    test {
+        maxHeapSize = "4g"
+        jvmArgs(
+            "--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED",
+            "--add-exports", "java.base/jdk.internal.util=ALL-UNNAMED",
+            "--add-exports", "java.base/sun.security.action=ALL-UNNAMED"
+        )
+    }
 }
 
 group = "ru.itmo.mpp"
@@ -11,14 +22,14 @@ repositories {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
 dependencies {
     implementation(kotlin("reflect"))
     testImplementation(kotlin("test-junit"))
-    testImplementation("org.jetbrains.kotlinx:lincheck:2.23")
+    testImplementation("org.jetbrains.kotlinx:lincheck:2.34")
 }
 
 sourceSets.main {
